768be7854de460c603537f5f2d92d33b0e67846e
Jeonghyeon Lee <hyeeon02@gmail.com>
2025-08-28 23:30:57 +0900

    docs 폴더 내 파일 복구

--- src/App.js
+++ src/App.js
@@ -34,3 +34,40 @@ function App() {
 }
 
 export default App;
+
+
+// import React from 'react';
+// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// import { CartProvider } from './context/CartContext';
+// import { CardProvider } from './context/CardContext';
+// import GlobalStyle from './styles/GlobalStyles';
+// import ProductListScreen from './screens/ProductListScreen';
+// import ProductDetailScreen from './screens/ProductDetailScreen';
+// import CartScreen from './screens/CartScreen';
+// import OrderCompleteScreen from './screens/OrderCompleteScreen';
+// import CardListScreen from './screens/CardListScreen';
+// import AddCardScreen from './screens/AddCardScreen';
+// import PaymentDetailScreen from './screens/PaymentDetailScreen';
+
+// function App() {
+//   return (
+//     <Router>
+//       <GlobalStyle />
+//       <CartProvider>
+//         <CardProvider>
+//           <Routes>
+//             <Route path="/" element={<ProductListScreen />} />
+//             <Route path="/product/:id" element={<ProductDetailScreen />} />
+//             <Route path="/cart" element={<CartScreen />} />
+//             <Route path="/order-complete" element={<OrderCompleteScreen />} />
+//             <Route path="/payment/cards" element={<CardListScreen />} />
+//             <Route path="/payment/add-card" element={<AddCardScreen />} />
+//             <Route path="/payment/items" element={<PaymentDetailScreen />} />
+//           </Routes>
+//         </CardProvider>
+//       </CartProvider>
+//     </Router>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -15,3 +15,42 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { HashRouter } from 'react-router-dom';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { HashRouter } from 'react-router-dom';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
+
